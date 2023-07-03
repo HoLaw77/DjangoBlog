8003d796ee0bb3ad90bbec61273c8d2409b0d81d
HoLaw77 <>
2023-07-03 16:58:41 +0000

    correct secret_key

--- codestar/settings.py
+++ codestar/settings.py
@@ -17,7 +17,6 @@ import dj_database_url
 
 
 if os.path.isfile('env.py'):
-
     import env
 
 
@@ -133,12 +132,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASH_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-DEFAULT_FILE_STORAGE = 'Cloudinary_storage.storage.MediaCloudinaryStorage'
-
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
