42ae22a960b5271e8243ece6dace230c70ce4cd5
HoLaw77 <>
2023-07-04 15:44:19 +0000

    adjust allowed hosts in settings.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['i-think-thereforeblog.heroku.com', 'localhost']
+ALLOWED_HOSTS = ['i-think-thereforeblog-b6a6384d9afe.herokuapp.com', 'localhost']
 
 
 # Application definition
